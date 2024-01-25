ffe07d27154161c1c939daf73819fe333933282b
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2024-01-25 09:18:28 +0200

    Adding user Entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,41 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class User {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+  @ApiProperty()
+  @Column()
+  name: string;
+  @ApiProperty()
+  @Column()
+  userId: string;
+  @ApiProperty({ required: false })
+  @Column()
+  bio?: string;
+  @ApiProperty({ required: false })
+  @Column()
+  mobileNumber?: string;
+  @ApiProperty()
+  @Column()
+  password: string;
+  @ApiProperty({ required: false })
+  @Column()
+  gender?: string;
+  @ApiProperty({ required: false })
+  @Column('date')
+  dateOfBirth?: Date;
+  @ApiProperty({ required: false })
+  @Column()
+  country?: string;
+  @ApiProperty()
+  @Column()
+  titleDisplay: string;
+  @ApiProperty()
+  @Column()
+  displayLanguage: string;
+  @ApiProperty()
+  @Column()
+  ratingsPrivacy: string;
+}
