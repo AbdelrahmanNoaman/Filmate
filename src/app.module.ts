bcbf6e2887a3ac301e676e0b749dca2347dcf0c8
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2024-01-25 08:18:14 +0200

    initializing actors resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ActorsModule } from './actors/actors.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ActorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
