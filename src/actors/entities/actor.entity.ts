3c71e4d4ce942653e944a3620555a2835869fedc
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2024-01-25 09:58:22 +0200

    Adding Actors entity

--- src/actors/entities/actor.entity.ts
+++ src/actors/entities/actor.entity.ts
@@ -1,1 +1,36 @@
-export class Actor {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity('actors')
+export class Actor {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column()
+  name: string;
+
+  @ApiProperty()
+  @Column({
+    type: 'date',
+  })
+  dateOfBirth: Date;
+
+  @ApiProperty({required:false})
+  @Column({
+    type: 'float4',
+  })
+  height?: number;
+
+  @ApiProperty({required:false})
+  @Column()
+  picture?: string;
+
+  @ApiProperty()
+  @Column()
+  summary: string;
+
+  @ApiProperty()
+  @Column()
+  countryOfOrigin: string;
+}
