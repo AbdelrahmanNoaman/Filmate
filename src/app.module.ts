01879c3c64c2e0e9050f2cff8ed1d75f93d0edc1
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2024-01-25 08:18:47 +0200

    initializing films resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ActorsModule } from './actors/actors.module';
+import { FilmsModule } from './films/films.module';
 
 @Module({
-  imports: [UsersModule, ActorsModule],
+  imports: [UsersModule, ActorsModule, FilmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
