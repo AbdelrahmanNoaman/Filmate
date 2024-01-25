cb04b700f54718886e24b9891cd20a06efcb3642
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2024-01-25 08:19:54 +0200

    initializing episodes resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { ActorsModule } from './actors/actors.module';
 import { FilmsModule } from './films/films.module';
 import { TvShowsModule } from './tv-shows/tv-shows.module';
+import { EpisodesModule } from './episodes/episodes.module';
 
 @Module({
-  imports: [UsersModule, ActorsModule, FilmsModule, TvShowsModule],
+  imports: [UsersModule, ActorsModule, FilmsModule, TvShowsModule, EpisodesModule],
   controllers: [AppController],
   providers: [AppService],
 })
